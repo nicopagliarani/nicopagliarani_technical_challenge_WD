68963530545774f46a75dc6eac85646ba1f55782
nicopagliarani <nico.pagliarani@gmail.com>
2022-04-28 15:38:21 +0200

    Iteration 1 done

---
+++ api-server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const fs = require('fs');
+var cors = require('cors');
+
+app.use(cors());
+
+let phonesFromApi = JSON.parse(fs.readFileSync('./phones.json'));
+
+app.get('/phones', (req, res) => {
+  res.send(phonesFromApi);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/phones`);
+});
